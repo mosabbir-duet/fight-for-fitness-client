f8239b1a5115aef9c63e90d67214349086142e9c
mosabbir.duet <mosabbir.duet@gmail.com>
2023-06-10 19:16:05 +0600

    Added setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2 className=" btn btn-warning">Hello</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>This is a header</div>;
+};
+
+export default Navbar;
